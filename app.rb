a1e361aaf044dbca3ff6b4e9a770f27a8ddf45b6
Robby Ranshous <rranshous@gmail.com>
2015-11-08 15:40:21 -0500

    refactor sinatra app out, threading

--- app.rb
+++ app.rb
@@ -1,45 +1,33 @@
-require 'sinatra'
-require_relative 'scraper'
 require_relative 'state'
-require_relative 'tumblr'
-
 require_relative 'background'
 
+require 'thread'
+Thread.abort_on_exception = true
+
+# create persistant state
 puts "creating state"
 state_path = File.join((ENV['DATA_DIR'] || './data'), 'skyscraper.lmc')
 state = State.new(state_path)
 puts "has state"
 
-post '/set_callback' do
-  callback = params[:callback].chomp
-  puts "setting callback: #{callback}"
-  state['callback'] = callback
-  content_type :text
-  redirect '/'
+background_thread = nil
+
+begin
+
+  # startup the background work
+  puts "starting background work"
+  background_thread = Thread.new do
+    background_loop state
+  end
+
+  # setup http front end
+  require_relative 'http'
+  puts "starting http front end"
+  http_interface state
+
+ensure
+  puts "stopping background thread"
+  background_thread.kill
 end
 
-post '/add_domain' do
-  new_domain = params[:new_domain].chomp
-  puts "adding domain: #{new_domain}"
-  existing_domains = state['domains'] || []
-  halt 409 if existing_domains.include? new_domain
-  state["domains"] = existing_domains + [new_domain]
-  redirect '/'
-end
-
-post '/remove_domain' do
-  to_remove = params[:domain].chomp
-  puts "removing: #{to_remove}"
-  existing_domains = state['domains'] || []
-  halt 409 unless existing_domains.delete to_remove
-  state['domains'] = existing_domains
-  redirect '/'
-end
-
-get '/' do
-  erb :index, locals: { state: state }
-end
-
-puts "starting background work"
-background_loop state
-
+puts "done in main"
--- background.rb
+++ background.rb
@@ -1,5 +1,5 @@
-require 'thread'
-Thread.abort_on_exception=true
+require_relative 'scraper'
+require_relative 'tumblr'
 
 SLEEP_TIME = (ENV['SLEEP_TIME'] || 60 * 5).to_i
 
@@ -9,67 +9,63 @@ def background_loop state
   puts "domains: #{state['domains']}"
   puts "starting thread"
 
+  image_client = Tumblr::Image.new
+  initial = true
 
-  Thread.new do
+  loop do
 
-    image_client = Tumblr::Image.new
-    initial = true
+    unless initial #ugh
+      puts "sleeping for #{SLEEP_TIME}s"
+      sleep SLEEP_TIME
+    end
+    initial = false
 
-    loop do
+    state['last_error'] = nil
+    puts "thread started"
 
-      unless initial #ugh
-        puts "sleeping for #{SLEEP_TIME}s"
-        sleep SLEEP_TIME
-      end
-      initial = false
+    domains = state['domains']
+    callback = state['callback']
+    if domains.nil? || domains.empty?
+      puts "no domains, halting"
+      state['last_error'] = "no domains set"
+      next
+    end
+    if callback.nil?
+      puts "no callback, halting"
+      state['last_error'] = "no callback set"
+      next
+    end
 
-      state['last_error'] = nil
-      puts "thread started"
-
-      domains = state['domains']
-      callback = state['callback']
-      if domains.nil? || domains.empty?
-        puts "no domains, halting"
-        state['last_error'] = "no domains set"
-        next
-      end
-      if callback.nil?
-        puts "no callback, halting"
-        state['last_error'] = "no callback set"
-        next
-      end
-
-      sm = ScrapeMaster.new state['domains']
-      puts "starting image details scraper"
-      sm.start do |key, image_details|
-        if state[key] && state[key]["processed"]
-          puts "has already been processed"
-          true
-        else
-          state[key] = image_details.merge({ processed: false })
-          ext = state[key]["ext"]
-          data = image_client.download(state[key]["href"])
-          begin
-            puts "posting"
-            post_domain = URI(image_details[:post_href]).host
-            rsp = HTTParty.post("#{callback}/#{post_domain}-#{key}.#{ext}", body: data)
-            if rsp.code == 200
-              puts "setting processed #{key}"
-              state[key] = state[key].merge({ processed: true })
-            else
-              puts "bad response: #{rsp.code}, halting"
-              state['last_error'] = "Bad HTTP Response: #{rsp.code}"
-              false
-            end
-          rescue StandardError => ex
-            puts "error posting: #{ex}, halting"
-            state['last_error'] = "Runtime Error: #{ex}"
+    sm = ScrapeMaster.new state['domains']
+    puts "starting image details scraper"
+    sm.start do |key, image_details|
+      if state[key] && state[key]["processed"]
+        puts "has already been processed"
+        true
+      else
+        state[key] = image_details.merge({ processed: false })
+        ext = state[key]["ext"]
+        data = image_client.download(state[key]["href"])
+        begin
+          puts "posting"
+          post_domain = URI(image_details[:post_href]).host
+          rsp = HTTParty.post("#{callback}/#{post_domain}-#{key}.#{ext}", body: data)
+          if rsp.code == 200
+            puts "setting processed #{key}"
+            state[key] = state[key].merge({ processed: true })
+          else
+            puts "bad response: #{rsp.code}, halting"
+            state['last_error'] = "Bad HTTP Response: #{rsp.code}"
             false
           end
-          true
+        rescue StandardError => ex
+          puts "error posting: #{ex}, halting"
+          state['last_error'] = "Runtime Error: #{ex}"
+          false
         end
+        true
       end
-      puts "done with scrape"
     end
+    puts "done with scrape"
   end
 end
